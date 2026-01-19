7e65b558bd2c9c310018b9eb66dc1d2cf6688764
fermeridamagni <hello@magni.dev>
2026-01-19 15:12:07 -0600

    fix: updated astro config to support github pages

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,11 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://fermeridamagni.github.io",
+  base: "/astro-snowfall",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
