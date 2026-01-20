aab31a391e8d7508cfd10f636d1532994f07c06d
fermeridamagni <hello@magni.dev>
2026-01-20 14:44:30 -0600

    fix: replaces cva to tailwind-variants

--- src/components/animate-ui/components/buttons/copy.tsx
+++ src/components/animate-ui/components/buttons/copy.tsx
@@ -6,42 +6,40 @@ import {
 } from "@components/animate-ui/primitives/buttons/button";
 import { useControlledState } from "@hooks/use-controlled-state";
 import { cn } from "@magnidev/tailwindcss-utils";
-import { cva, type VariantProps } from "class-variance-authority";
 import { CheckIcon, CopyIcon } from "lucide-react";
 import { AnimatePresence, motion } from "motion/react";
 import React from "react";
+import { tv, type VariantProps } from "tailwind-variants";
 
-const buttonVariants = cva(
-  "flex shrink-0 items-center justify-center rounded-md outline-none transition-[box-shadow,_color,_background-color,_border-color,_outline-color,_text-decoration-color,_fill,_stroke] focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
-  {
-    variants: {
-      variant: {
-        default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
-        accent: "bg-accent text-accent-foreground shadow-xs hover:bg-accent/90",
-        destructive:
-          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:bg-destructive/60 dark:focus-visible:ring-destructive/40",
-        outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50",
-        secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
-        ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
-      },
-      size: {
-        default: "size-9",
-        xs: "size-7 rounded-md [&_svg:not([class*='size-'])]:size-3.5",
-        sm: "size-8 rounded-md",
-        lg: "size-10 rounded-md",
-      },
+const buttonVariants = tv({
+  base: "flex shrink-0 items-center justify-center rounded-md outline-none transition-[box-shadow,_color,_background-color,_border-color,_outline-color,_text-decoration-color,_fill,_stroke] focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+  variants: {
+    variant: {
+      default:
+        "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+      accent: "bg-accent text-accent-foreground shadow-xs hover:bg-accent/90",
+      destructive:
+        "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:bg-destructive/60 dark:focus-visible:ring-destructive/40",
+      outline:
+        "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50",
+      secondary:
+        "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+      ghost:
+        "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+      link: "text-primary underline-offset-4 hover:underline",
     },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
+    size: {
+      default: "size-9",
+      xs: "size-7 rounded-md [&_svg:not([class*='size-'])]:size-3.5",
+      sm: "size-8 rounded-md",
+      lg: "size-10 rounded-md",
     },
-  }
-);
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
 
 type CopyButtonProps = Omit<ButtonPrimitiveProps, "children"> &
   VariantProps<typeof buttonVariants> & {
